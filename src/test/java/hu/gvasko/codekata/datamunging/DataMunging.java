3bb1039bce3b3af2fc42a39e6e3b2e7746292e95
Gabor Vasko-Szedlar <gvasko@chemaxon.com>
2015-05-08 18:10:49 +0200

    Add football test -- and it fails

--- src/test/java/hu/gvasko/codekata/datamunging/DataMunging.java
+++ src/test/java/hu/gvasko/codekata/datamunging/DataMunging.java
@@ -14,16 +14,31 @@ import java.net.URL;
  */
 public class DataMunging {
 
-    // Columns
-    final int WEATHER_DAY_LEN = 5;
-    final int WEATHER_MAX_T_LEN = 6;
-    final int WEATHER_MIN_T_LEN = 6;
-    final String WEATHER_DAY_NAME = "Dy";
-    final String WEATHER_MAX_T_NAME = "MxT";
-    final String WEATHER_MIN_T_NAME = "MnT";
+    // Weather Columns
+    static final int WEATHER_DAY_LEN = 5;
+    static final int WEATHER_MAX_T_LEN = 6;
+    static final int WEATHER_MIN_T_LEN = 6;
+    static final String WEATHER_DAY_NAME = "Dy";
+    static final String WEATHER_MAX_T_NAME = "MxT";
+    static final String WEATHER_MIN_T_NAME = "MnT";
+
+    // Football Columns
+    static final int FOOTBALL_NUM_LEN = 7;
+    static final int FOOTBALL_TEAM_LEN = 16;
+    static final int FOOTBALL_P_LEN = 6;
+    static final int FOOTBALL_W_LEN = 4;
+    static final int FOOTBALL_L_LEN = 4;
+    static final int FOOTBALL_D_LEN = 6;
+    static final int FOOTBALL_F_LEN = 4;
+    static final int FOOTBALL_MINUS_LEN = 3;
+    static final int FOOTBALL_A_LEN = 6;
+    static final int FOOTBALL_PTS_LEN = 3;
+    static final String FOOTBALL_TEAM_NAME = "Team";
+    static final String FOOTBALL_GOALS_FOR_NAME = "F";
+    static final String FOOTBALL_GOALS_AGAINST_NAME = "A";
 
     @Test
-    public void testDayOfSmallestTemperatureSpread() throws Exception {
+    public void testDayWithSmallestTemperatureSpread() throws Exception {
         URL datFile = this.getClass().getResource("weather.dat");
         try (StringTableParser parser = StringTableFactory.getInstance().getParser(datFile.toURI())) {
             StringTable table = parser.firstRowIsHeader().excludeLastRow().excludeEmptyRows().parse(
@@ -36,6 +51,23 @@ public class DataMunging {
             StringRecord actualRecord = DataMungingUtil.getFirstMinDiffRecord(table.getAllRecords(), WEATHER_MAX_T_NAME, WEATHER_MIN_T_NAME);
             String actualDay = actualRecord.get(WEATHER_DAY_NAME);
             Assert.assertEquals(dayOfSmallestTemperatureSpread, actualDay);
+        }
+    }
+
+    @Test
+    public void testNameOfTeamWithSmallestGoalDifference() throws Exception {
+        URL datFile = this.getClass().getResource("football.dat");
+        try (StringTableParser parser = StringTableFactory.getInstance().getParser(datFile.toURI())) {
+            StringTable table = parser.firstRowIsHeader().excludeEmptyRows().parse(
+                    FOOTBALL_NUM_LEN, FOOTBALL_TEAM_LEN, FOOTBALL_P_LEN, FOOTBALL_W_LEN,
+                    FOOTBALL_L_LEN, FOOTBALL_D_LEN, FOOTBALL_F_LEN, FOOTBALL_MINUS_LEN,
+                    FOOTBALL_A_LEN, FOOTBALL_PTS_LEN
+            );
+
+            String nameOfTeamWithSmallestGoalDifference = "Aston_Villa";
+            StringRecord actualRecord = DataMungingUtil.getFirstMinDiffRecord(table.getAllRecords(), FOOTBALL_GOALS_FOR_NAME, FOOTBALL_GOALS_AGAINST_NAME);
+            String actualTeamName = actualRecord.get(FOOTBALL_TEAM_NAME);
+            Assert.assertEquals(nameOfTeamWithSmallestGoalDifference, actualTeamName);
         }
     }
 }
