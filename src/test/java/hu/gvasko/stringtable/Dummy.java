773e03193747c8fa1f259789a67a2817e3674cc4
Gabor Vasko-Szedlar <gvasko@chemaxon.com>
2015-05-18 19:48:41 +0200

    Refactoring: separating isolated and integration tests (backup)

--- src/test/java/hu/gvasko/stringtable/Dummy.java
+++ src/test/java/hu/gvasko/stringtable/Dummy.java
@@ -6,6 +6,9 @@ import org.junit.Test;
  * Created by Gvasko on 2015.05.18..
  */
 public class Dummy {
+
+    // TODO: move these tests to the right place
+
     @Test(expected = IllegalStateException.class)
     public void emptyRecordCannotBeCreatedThrowsException() {
         StringTableFactory stringTableFactory = StringTableFactory.getInstance();
---
+++ src/test/java/hu/gvasko/stringtable/integration/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package should contain: test fixture classes and (isolated) unit tests.
+ * Created by Gvasko on 2015.05.18..
+ */
+package hu.gvasko.stringtable.integration;
